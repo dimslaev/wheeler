73e8d5f9e2bbc470350002efd44b647d6f41031b
Dimitar Slaev <dimitar@plat4m.com>
2020-02-24 10:03:39 +0200

    update scroll function

--- example/index.js
+++ example/index.js
@@ -32,36 +32,10 @@ window.addEventListener("load", function() {
     wings[1].style.transform = "translateX(0)";
   };
 
-  var scrollY = function(y, speed = 600) {
-    var start = window.pageYOffset;
-    var change = y - start;
-    var currentTime = 0;
-    var increment = 20;
-
-    var easeInOutCubic = function(t, b, c, d) {
-      if ((t /= d / 2) < 1) return (c / 2) * t * t * t + b;
-      return (c / 2) * ((t -= 2) * t * t + 2) + b;
-    };
-
-    var animateScroll = function() {
-      var val = y;
-      currentTime += increment;
-      val = easeInOutCubic(currentTime, start, change, speed);
-
-      window.scrollTo(0, val);
-      if (currentTime < speed) {
-        setTimeout(animateScroll, increment);
-      }
-    };
-    setTimeout(animateScroll, 0);
-  };
-
-  scrollY(0, 0);
-
   Wheeler.init({
     duration: 600,
     minScrollCount: 0,
-    maxScrollCount: 3,
+    maxScrollCount: 2,
     onScrollStart: function(scrollDirection, scrollCount) {
       console.log(
         `scroll direction: ${scrollDirection} | scrollCount: ${scrollCount}`,
@@ -73,7 +47,7 @@ window.addEventListener("load", function() {
         }
 
         if (scrollCount > 0) {
-          scrollY(window.innerHeight * scrollCount);
+          smoothScrollTo("#page-" + (scrollCount + 1));
         }
       }
       if (scrollDirection === "up") {
@@ -81,11 +55,10 @@ window.addEventListener("load", function() {
           // do nothing
         } else if (scrollCount === 1) {
           page1AnimationReverse();
-        } else if (scrollCount === 3) {
-          scrollY(0);
-          page1AnimationReverse();
+        } else if (scrollCount === 2) {
+          smoothScrollTo("#page-1");
         } else {
-          scrollY(window.innerHeight * (scrollCount - 2));
+          smoothScrollTo("#page-" + (scrollCount - 1));
         }
       }
     },
@@ -93,8 +66,11 @@ window.addEventListener("load", function() {
       console.log(
         `scroll direction: ${scrollDirection} | scrollCount: ${scrollCount}`,
       );
-      if (window.pageYOffset === 0 && scrollCount === 2)
+
+      if (scrollDirection === "up" && scrollCount === 1) {
+        page1AnimationReverse();
         Wheeler.setScrollCount(0);
+      }
     },
   });
 });
